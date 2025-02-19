c07931c66c5cc4112073de27d1136455afc3828e
Asta1984 <salilmandal908@gmail.com>
2025-02-19 16:52:44 +0530

    by_generation

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
-const express = require("express")
-const axios = require('axios');
+const express = require("express");
+const axios = require("axios");
 
+const app = express();
+const port = 8000;
 
-const app = express()
-const port = 8000
+app.get("/generation/:id", async (req, res) => {
+    try {
+        const id = req.params.id;
+        console.log(`Fetching data for Generation ${id}...`); // Debug log
 
-app.get("/pokemon/:name", async (req, res) => {
-    try{
-        const pokemonName = req.params.name.toLowerCase();
-        const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
-        res.json(response.data)
-    }
-    catch (error) {
-        res.status(500).json({error: "Pokemon not found or API error"})
+        const response = await axios.get(`https://pokeapi.co/api/v2/generation/${id}`);
+        
+        res.json(response.data);
+    } catch (error) {
+        console.error("Error fetching data:", error.message); // Log the actual error
+        res.status(500).json({ error: "Pokemon generation not found or API error" });
     }
 });
 
 app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
-})
+    console.log(`Server listening on http://localhost:${port}`);
+});
